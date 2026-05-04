2ef903cad8b0cfda38be6e79b61a3db2f02a0eb6
2025020032 <soomin1316@gmail.com>
2026-05-04 15:55:28 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    bool k=false;
+
+    scanf("%d",&N);
+
+    for (int a=1;a*900<=N;a++){
+        for (int b=2;b*750<=N;b+=2){ 
+            for (int c=1;c*200<=N;c++){
+                
+                
+                if ((a*900+b*750+c*200)==N){
+                    
+                    
+                    if (c<a||c<b){
+                        printf("%d %d %d\n",a,b,c);
+                        k=true; 
+                    }
+                }
+            }
+        }
+    }
+
+    if(k!=true) { 
+        printf("None\n");
+    }
+    return 0;
+    }
